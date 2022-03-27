a5a6e161f2bc4db000733dbf9e95081ae528abac
#codename$ <95187806+sakib4535@users.noreply.github.com>
2022-03-27 16:33:16 +0600

    Add files via upload

---
+++ sorted.py
@@ -0,0 +1,30 @@
+pale = "I am very busy because I am working now!"
+sigma = "138918.2109381-.23478394.2314. He is Nor workingsasas"
+
+sort = sorted(pale)
+sort_1 = sorted(sigma)
+print(sort)
+print(sort_1)
+
+number = [8.7, 5.9, 3.4, 6.7, 1.2, 2.3, 9.8]
+h = sorted(number)
+print(h)
+
+number.sort()    #Dont't go for another_number_point kind variable
+print(number)
+
+missing_letter = sorted("iashcgYTDyiblqwbyvdyyY saudyauihg&YTT #Yyt67da",
+                  key=str.casefold)
+print(missing_letter)
+
+name = ["mash",
+        "mushi",
+        "shek",
+        "mam",
+        "aal",
+        "Mash",
+        "aAl"
+        ]
+name.sort(key=str.casefold)
+print(name)
+
